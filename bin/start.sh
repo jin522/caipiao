89c9667abcd75d7ab3bb8bd5cd6b390165e346d7
wjp <wjp_199502@163.com>
2024-12-19 16:27:44 +0800

    fix: 优化入口脚本及日志文件生成

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-which_python="/home/manager/.conda/envs/manager/bin/python"
+versions=("python" "python2" "python3")
+
+for version in "${versions[@]}"
+do
+    if command -v "$version" &> /dev/null
+    then
+        python_path=$(which "$version")
+        python_dir=$(dirname "$python_path")
+        which_python=$python_dir/$version
+    fi
+done
+
 current_dir=$(dirname "$(dirname "$0")")
 cmd=$1
 ${which_python} $current_dir/lib/main.py $cmd 2>&1 &
--- lib/utils/log_config.py
+++ lib/utils/log_config.py
@@ -1,9 +1,14 @@
 import logging as LOG
-
+from pathlib import Path
 
 def getLog(log_file_name):
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
     DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
+    file_path = Path(log_file_name)
+    if not file_path.exists():
+        # 文件不存在，则创建文件（包括目录）
+        file_path.parent.mkdir(parents=True, exist_ok=True)  # 创建目录（如果不存在）
+        file_path.touch()  # 创建空文件
     fp = LOG.FileHandler(str(log_file_name), encoding='utf-8')
     LOG.basicConfig(level=LOG.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp])
     return LOG
